4239237d8a7b9b01438b8924a1a3c45868127c58
SeokHui Jo <suchie777@gmail.com>
2023-09-28 00:57:57 +0900

    Update DFS와 BFS.cc

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -4,23 +4,27 @@
 #include <algorithm>
 using namespace std;
 
-void dfs(int v, vector<int>& ans, vector<bool>& check, vector<vector<int>>g)
+const int MAX = 1001;
+
+bool check[MAX];
+
+void dfs(int v, vector<int> g[])
 {
-	ans.push_back(v);
+	cout << v << " ";
 	check[v] = true;
 	for (auto n : g[v])
 	{
 		if (!check[n])
 		{
-			dfs(n, ans, check, g);
+			dfs(n, g);
 		}
 	}
 
 }
-void bfs(int v, vector<int>& ans, vector<bool>& check, vector<vector<int>>g)
+void bfs(int v, vector<int> g[])
 {
 
-	ans.push_back(v);
+	cout << v << " ";
 	queue<int> q;
 	check[v] = 1;
 	q.push(v);
@@ -36,7 +40,7 @@ void bfs(int v, vector<int>& ans, vector<bool>& check, vector<vector<int>>g)
 			{
 				q.push(i);
 				check[i] = true;
-				ans.push_back(i);
+				cout << i << " ";
 			}
 		}
 	}
@@ -46,37 +50,29 @@ int main()
 {
 	int n, m, v;
 	cin >> n >> m >> v;
-	vector<vector<int>> graph(n + 1);
-	vector<int> dfsAns;
-	vector<bool> dfsCheck(n + 1, false);
-	vector<int> bfsAns;
-	vector<bool> bfsCheck(n + 1, false);
+
+	vector<int> graph[MAX];
 
 	for (int i = 0; i < m; ++i)
 	{
 		int s, e;
 		cin >> s >> e;
-		graph[s].push_back(e);
-		graph[e].push_back(s);
+		graph[s].emplace_back(e);
+		graph[e].emplace_back(s);
 	}
 
-	for (auto& g : graph)
+	for (int i = 1; i <= n;++i)
 	{
-		std::sort(g.begin(), g.end());
+		sort(graph[i].begin(), graph[i].end());
 	}
 
-
-	dfs(v, dfsAns, dfsCheck, graph);
-	bfs(v, bfsAns, bfsCheck, graph);
-
-
-	for (auto n : dfsAns)
-	{
-		cout << n << " ";
-	}
+	dfs(v, graph);
 	cout << endl;
-	for (auto n : bfsAns)
+
+	for (int i = 1;i <= n;++i)
 	{
-		cout << n << " ";
+		check[i] = false;
 	}
+	bfs(v, graph);
+
 }
