53e8f50000bd30cfa53654dfae4fea8f9ce15f99
SeokHui Jo <suchie777@gmail.com>
2023-10-15 21:29:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(vector<int> a , vector<int> b)
+{
+    return a[2] < b[2];
+}
+
+int find(vector<int>&v , int n)
+{
+    if(v[n] == n)
+        return n;
+    else
+        return v[n] = find(v,v[n]);
+}
+
+void unionParent(vector<int>&v, int a, int b)
+{
+    //이미 앞에서 루트노드를 찾고 호출하기때문에 생략가능
+    a = find(v,a);
+    b = find(v,b);
+    //더작은쪽을 부모로함
+    a < b ? v[b] = a : v[a] = b;
+}
+
+bool isConnect(vector<int>&v, int a, int b)
+{
+    int start = find(v,a);
+    int end = find(v,b);
+    
+    return start == end;
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    //각노드의 부모를 저장하는벡터
+    vector<int> root(n+1);
+    
+    for(int i=0; i<=n; ++i)
+    {
+        //기본적으로 자기자신으로 초기화
+        root[i] = i;
+    }
+    
+    sort(costs.begin(),costs.end(), compare);
+    
+    for(auto c : costs)
+    {
+        int start = find(root,c[0]);
+        int end = find(root,c[1]);
+        
+        //시작노드와 끝노드의 부모가 같지않은경우 = 연결이 안되있음
+        if(!isConnect(root,c[0],c[1]))
+        {
+            answer += c[2];
+            unionParent(root,c[0],c[1]);
+        }
+        
+    }
+    
+    return answer;
+}
