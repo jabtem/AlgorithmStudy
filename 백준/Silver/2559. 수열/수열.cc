2a7e5eea10ed00b13664bea8e5447cd5415394be
SeokHui Jo <suchie777@gmail.com>
2023-11-16 01:09:38 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,24 +1,45 @@
 #include <iostream>
-#include <algorithm>
-#include <climits>
 #include <vector>
+#include <algorithm>
 using namespace std;
-int answer = INT_MIN;
-int main() {
-	int start, end, n, k, sum = 0;
-	cin >> n >> k; 
-	start = 0;
-	end = k - 1;
-	vector <int> v(n); 
-	for (int i = 0; i < n; i++)cin >> v[i]; 
-	for (int i = 0; i <= k - 1; i++)sum += v[i]; 
-	for (int i = 0; i <= n - k; i++) {
-		answer = max(answer, sum); 
-		if (end == n - 1)break; 
-		else {	
-			sum -= v[start++]; 
-			sum += v[++end]; 
+
+
+void solution(int k, vector<int> & numbers)
+{
+	int result = -10000001;
+	int s = 0;
+	int e = k-1;
+	int sum = 0;
+
+	for (int i = 0; i <= k - 1; ++i)
+	{
+		sum += numbers[i];
+	}
+
+	while (e < numbers.size())
+	{
+		result = max(result, sum);
+		if (e == numbers.size() - 1)
+			break;
+		else
+		{
+			sum -= numbers[s++];
+			sum += numbers[++e];
 		}
 	}
-	cout << answer; 
+
+	cout << result;
+}
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> numbers(n, 0);
+	for (int i = 0; i < n;++i)
+	{
+		cin >> numbers[i];
+	}
+
+	solution(k, numbers);
 }
