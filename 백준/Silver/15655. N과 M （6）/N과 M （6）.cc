7c1073d4707e3beb6571bf0380e372044dd3b88a
SeokHui Jo <suchie777@gmail.com>
2023-11-16 18:32:40 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void Combination(vector<int>arr, vector<int>comb, int index = 0, int depth = 0)
+{
+	if (depth == comb.size())
+	{
+		for (int i = 0; i < comb.size();++i)
+			cout << comb[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = index; i < arr.size();++i)
+	{
+		comb[depth] = arr[i];
+		Combination(arr, comb, i + 1, depth + 1);
+
+	}
+}
+
+
+int main()
+{
+	int n, r;
+	cin >> n >> r;
+
+	vector<int> arr(n,0);
+	vector<int> perm(r);
+	
+	for (int i = 0; i < n;++i)
+	{
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+
+	Combination(arr, perm);
+}
