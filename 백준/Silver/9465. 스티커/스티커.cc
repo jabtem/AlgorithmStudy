c2213e32660f5e2d92642ad039b5de83c26d0491
SeokHui Jo <suchie777@gmail.com>
2024-02-06 04:47:26 +0900

    Update 스티커.cc

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,41 +1,42 @@
 #include <iostream>
-#include <cstdio>
- 
+#include <vector>
+#include <algorithm>
 using namespace std;
- 
-const int MAXN = 100000;
- 
-int buf[2][MAXN + 1] = { 0, };
-int dp[2][MAXN + 1] = { 0, };
- 
-int max(int a, int b) { if (a > b) return a; return b; }
- 
-int bottomUp(int n) {
-    for (int i = 0; i < n; i++) 
-        scanf("%d", &buf[0][i]);
- 
-    for (int i = 0; i < n; i++)
-        scanf("%d", &buf[1][i]);
- 
-    dp[0][0] = buf[0][0];
-    dp[1][0] = buf[1][0];
-    dp[0][1] = buf[0][1] + dp[1][0];
-    dp[1][1] = buf[1][1] + dp[0][0];
- 
-    for (int i = 2; i < n; i++) {
-        dp[0][i] = buf[0][i] + max(dp[1][i - 1], dp[1][i - 2]);
-        dp[1][i] = buf[1][i] + max(dp[0][i - 1], dp[0][i - 2]);
-    }
- 
-    return max(dp[0][n - 1], dp[1][n - 1]);
-}
- 
-int main() {
-    int t, n;
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d", &n);
-        printf("%d\n", bottomUp(n));
-    }
-    return 0;
+
+int dp[2][100001] = { 0, };
+int main() 
+{
+	int T, n;
+
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> n;
+		vector<int> vec[2];
+		int lastIndex = n - 1;
+		for (int i = 0; i < 2;++i)
+		{
+			for (int j = 0; j < n;++j)
+			{
+				int num;
+				cin >> num;
+				vec[i].push_back(num);
+			}
+		}
+
+		dp[0][0] = vec[0][0];
+		dp[1][0] = vec[1][0];
+		dp[0][1] = vec[0][1] + dp[1][0];
+		dp[1][1] = vec[1][1] + dp[0][0];
+
+		for (int i = 2; i < n;++i)
+		{
+			dp[0][i] = vec[0][i] + max(dp[1][i - 1], dp[1][i - 2]);
+			dp[1][i] = vec[1][i] + max(dp[0][i - 1], dp[0][i - 2]);
+		}
+
+		int result =  max(dp[0][lastIndex], dp[1][lastIndex]);
+		cout << result <<"\n";
+	}
 }
