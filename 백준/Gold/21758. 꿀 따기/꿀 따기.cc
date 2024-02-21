c8bc6c3705614b5249b5946ccaab3fd9b421f244
SeokHui Jo <suchie777@gmail.com>
2024-02-22 02:46:58 +0900

    [Gold V] Title: 꿀 따기, Time: 40 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.cc
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -10,24 +10,26 @@ int main()
 	int N;
 	cin >> N;
 
-	vector<int> vec(N, 0);
+	vector<int> honey(N + 1, 0);
+	vector<int> sum(N + 1, 0);
 
-	for (int i = 0; i < N;++i)
+	for (int i = 1; i <= N;++i)
 	{
-		cin >> vec[i];
+		cin >> honey[i];
+		sum[i] = sum[i - 1] + honey[i];
 	}
 	int result = 0;
 
-	for (int i = 1; i < N - 1;++i)
+	for (int i = 2; i < N; ++i)
 	{
 		//벌 벌 꿀통
-		int value1 = accumulate(vec.begin() + 1, vec.end(), 0) - vec[i] + accumulate(vec.begin() + i + 1, vec.end(), 0);
+		int value1 = (sum[N] - sum[1] - honey[i]) + (sum[N] - sum[i]);
 		//꿀통 벌 벌
-		int value2 = accumulate(vec.begin(), vec.end() - 1, 0) - vec[N - 1 - i] + accumulate(vec.begin(), vec.end() - 1 - i, 0);
+		int value2 = (sum[N - 1] - honey[i]) + sum[i - 1];
 		//벌 꿀통 벌
-		int value3 = accumulate(vec.begin() + 1, vec.begin() + 1 + i, 0) + accumulate(vec.begin() + i, vec.end() - 1, 0);
-		
-		result = max(result,max(max(value1, value2),value3));
+		int value3 = (sum[i] - honey[1]) + (sum[N - 1] - sum[i - 1]);
+
+		result = max(result, max(max(value1, value2), value3));
 	}
 
 	cout << result << "\n";
